9c0d3ab82f0beae3bc491be7c63111ffb57a40be
Sahilthakur78 <sahilthakur78902u@gmail.com>
2026-03-02 21:13:10 +0530

    redesigned

--- reactrouter/src/components/header/header.jsx
+++ reactrouter/src/components/header/header.jsx
@@ -34,13 +34,21 @@ export default function Header() {
                         <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
                             <li>
                                 <NavLink
-                                    className={(isactive) =>
+                                    className={({isactive}) =>
                                         `block py-2 pr-4 pl-3 duration-200 ${isactive ?"text-orange-700":"text-gray-700"} border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
                                     }
                                 >
                                     Home
                                 </NavLink>
                             </li>
+                            <NavLink
+                            to="/about"
+                                    className={({isactive}) =>
+                                        `block py-2 pr-4 pl-3 duration-200 ${isactive ?"text-orange-700":"text-gray-700"} border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
+                                    }
+                                >
+                                    about
+                                </NavLink>
                         </ul>
                     </div>
                 </div>
